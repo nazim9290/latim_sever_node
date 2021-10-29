6d3fc87e688a14b30338b8ee5ba90ecce392a40e
Nazim Uddin <nushourav4@gmail.com>
2021-10-29 14:06:05 +0900

    initiali

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://ogro9290:3zpc5KYpujeWtMkr@cluster0.eanfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("travel_bd");
+    const tourservice = database.collection("tour");
+    const username = database.collection("user_name");
+    //custom api
+    app.get("/tour", async (req, res) => {
+      const cursor = tourservice.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+      res.json(services);
+    });
+
+    app.get("/home", (req, res) => {
+      res.send("mongo connect");
+    });
+
+    app.post("/tour", async (req, res) => {
+      const servic = req.body;
+      console.log(servic);
+      const result = await username.insertOne(servic);
+      res.json(result);
+    });
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+//custom api
+app.get("/", (req, res) => {
+  res.send("Hell!");
+});
+
+//applesten
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
